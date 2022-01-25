3588d342640b47b53544bc819847a2d3b38f85ac
jusurkin <61032491+jusurkin@users.noreply.github.com>
2022-01-25 12:36:45 -0500

    At home project #3

--- p03-flag-refactoring/p03-flag-refactor/GameView.swift
+++ p03-flag-refactoring/p03-flag-refactor/GameView.swift
@@ -9,12 +9,7 @@
 import SwiftUI
 
 struct GameView: View {
-    @State private var countries = CountryList.countries.shuffled().prefix(3)
-    @State private var correctAnswerIndex = Int.random(in: 0...2)
-    @State private var score = 0
-
-    @State private var showingAlert = false
-    @State private var alertTitle = ""
+    @StateObject var viewModel = GameViewModel()
     
     var body: some View {
         ZStack {
@@ -26,45 +21,35 @@ struct GameView: View {
             VStack {
                 Text("Tap the flag of")
                     
-                Text(targetCountry.name)
+                Text(viewModel.targetCountry.name)
                     .font(.largeTitle)
                     .fontWeight(.black)
                     
-                Text("Score: \(score)")
+                Text("Score: \(viewModel.score)")
 
                 Spacer()
-                    
-                ForEach(countries, id: \.id) { country in
+                
+                ForEach(viewModel.countries, id: \.id) { countryAnswer in
                     Button(action: {
-                        if country == targetCountry {
-                            alertTitle = "Correct"
-                            score += 1
-                        } else {
-                            alertTitle = "Wrong! Thats the flag of \(country)"
-                        }
-                        
-                        showingAlert = true
+                        viewModel.checkAnswer(country: countryAnswer)
+
+                        viewModel.showingAlert = true
                     }) {
-                        FlagImage(imageName: country.name)
+                        FlagImage(imageName: countryAnswer.name)
                     }
                 }
                     
                 Spacer()
             }
-            .alert(isPresented: $showingAlert) {
-                Alert(title: Text(alertTitle),
-                      message: Text("Your Score is \(score)"),
-                      dismissButton: .default(Text("Continue")) {
-                          countries = CountryList.countries.shuffled().prefix(3)
-                          correctAnswerIndex = Int.random(in: 0...2)
+            .alert(isPresented: $viewModel.showingAlert) {
+                Alert(title: Text(viewModel.alertTitle), message: Text("Your Score is \(viewModel.score)"), dismissButton: .default(Text("Continue")) {
+                        viewModel.shuffleCountries()
+                        
+                        viewModel.correctAnswerIndex = Int.random(in: 0...2)
                       })
             }
         }
         .preferredColorScheme(.dark)
-    }
-    
-    var targetCountry: Country {
-        return countries[correctAnswerIndex]
     }
 }
 
--- p03-flag-refactoring/p03-flag-refactor/GameViewModel.swift
+++ p03-flag-refactoring/p03-flag-refactor/GameViewModel.swift
@@ -7,4 +7,31 @@
 
 import Foundation
 
-#warning("Add GameViewModel class in this file")
+final class GameViewModel: ObservableObject {
+    @Published var countries = CountryList.countries.shuffled().prefix(3)
+    @Published var correctAnswerIndex = Int.random(in: 0...2)
+    @Published var score = 0
+
+    @Published var showingAlert = false
+    @Published var alertTitle = ""
+    
+    var targetCountry: Country {
+        return countries[correctAnswerIndex]
+    }
+    
+    func checkAnswer(country: Country) {
+        if country == targetCountry {
+            alertTitle = "Correct"
+            score += 1
+        } else {
+            alertTitle = "Wrong! Thats the flag of \(country.name)"
+        }
+        
+        showingAlert = true
+    }
+    
+    func shuffleCountries() {
+        countries = CountryList.countries.shuffled().prefix(3)
+    }
+}
+
